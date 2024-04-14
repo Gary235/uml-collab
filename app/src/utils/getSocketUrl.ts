7ddd742f61b68fb59fea3b129086f2c8449666f3
Gary235 <berkman.gary@gmail.com>
2024-04-14 16:17:12 -0300

    update favicon

--- app/src/utils/getSocketUrl.ts
+++ app/src/utils/getSocketUrl.ts
@@ -2,6 +2,5 @@ export default function(username: string, docId: string) {
   if (window.location.origin.includes('localhost'))
     return `ws://localhost:5432/doc/?username=${username}&doc=${docId}`
 
-  // return `wss://uml-collab-server.zeabur.app/doc/?username=${username}&doc=${docId}`
   return `wss://uml-collab.onrender.com/doc/?username=${username}&doc=${docId}`
 }
