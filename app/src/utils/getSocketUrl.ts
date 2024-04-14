2b199ed99fb52397d958be9eaa790fd302be5e04
Gary235 <berkman.gary@gmail.com>
2024-04-14 15:39:04 -0300

    use render url

--- app/src/utils/getSocketUrl.ts
+++ app/src/utils/getSocketUrl.ts
@@ -1,8 +1,7 @@
 export default function(username: string, docId: string) {
-  console.log(window.location.origin.includes('localhost'));
-
   if (window.location.origin.includes('localhost'))
     return `ws://localhost:5432/doc/?username=${username}&doc=${docId}`
 
-  return `wss://uml-collab-server.zeabur.app/doc/?username=${username}&doc=${docId}`
+  // return `wss://uml-collab-server.zeabur.app/doc/?username=${username}&doc=${docId}`
+  return `wss://uml-collab.onrender.com/doc/?username=${username}&doc=${docId}`
 }
